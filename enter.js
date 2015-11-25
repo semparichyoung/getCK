df588eeec483c7cc7053bfde086ef3647e168d8d
SheL <shelob5020@gmail.com>
2015-11-25 23:09:03 +0800

    url?page=maxPage

--- enter.js
+++ enter.js
@@ -2,14 +2,42 @@ $(function(e) {
 	$("#i").on("keypress", function(e) {
 		if(e.which == 13) {
 			var v = $("#i").val();
+			var ip = $("#input").remove();
+			var vary = v.split("=");
+			var maxPage = vary[vary.length - 1];
+			delete vary[vary.length - 1];
+			v = vary.join("=");
 			console.log(v);
-			$.get("get.php", {"url": v}, function(e) {
-				var ele = $(e);
-				var p = $(".plhin", ele);
-				console.log(p);
-				$("#area").html(p);
-				$(".plsTitle").remove();
-			});
+			getHtml(1);
+			function getHtml(i) {
+				console.log("get:" + v + i);
+				$.get("get.php", {"url": v + i}, function(e) {
+					var ele = $(e);
+					var p = $(".plhin", ele);
+					p.find(".plsTitle").remove();
+					p.find(".pob.cl.y").remove();
+					p.find(".pstatus").remove();
+					p.find("a").remove();
+					$("#area").append(p);
+					// $(".plsTitle").remove();
+					// $(".pob.cl.y").remove();
+					// $(".pstatus").remove();
+					console.log("page:" + i + " get!");
+					if(i < maxPage) {
+						getHtml(i + 1);
+					}else {
+						console.log("all done!!!");
+						$("#area").pageNav({
+							"content": $(".plhin"),
+							"showNum": 10,
+							"visPage": 7
+						});
+					}
+				});
+			}
+			function test(index) {
+				console.log(index);
+			}
 		}
 	});
 });
--- index.php
+++ index.php
@@ -4,13 +4,29 @@
 
 		<title>ck</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+		<script src="../pageNav/pageNav.js"></script>
 		<style type="text/css" media="all">
 			#input {
-				width: 50%;
-				margin: 50px auto;
+					width: 50%;
+					margin: 50px auto;
 			}
 			#i {
-				width: 100%;
+					width: 100%;
+			}
+			.invisible {
+					display: none;
+			}
+			.plhin {
+					margin: 40px auto;
+					width: 80%;
+			}
+			.t_f {
+					font-size: 25px;
+					line-height: 2.5;
+			}
+			nav {
+				text-align: center;
 			}
 		</style>
 	</head>
@@ -20,7 +36,7 @@
 			<input type="text" name="" id="i" value="" />
 		</div><!--END: . #input-->
 		<div id="area">
-			
+
 		</div><!-- END: #area -->
 	</body>
 	<script src="enter.js"></script>
